f2d95b51fdbc41b1de1d01812e0e9e858614b30f
Lustres <logansong@icloud.com>
2017-08-09 18:58:04 +0800

    :tada: Initial commit

---
+++ homebrew-services/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  homebrew-services
+//
+//  Created by Lustres on 8/9/17.
+//  Copyright © 2017 Lustres. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ homebrew-services/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  homebrew-services
+//
+//  Created by Lustres on 8/9/17.
+//  Copyright © 2017 Lustres. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
